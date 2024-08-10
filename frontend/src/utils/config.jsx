d39907e4b37b1e69e68bd064fbfdba788f454586
mosa beryal <beryalmosa@gmail.com>
2024-08-10 19:59:48 +0500

    products api implemented with pagination

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,13 +1,12 @@
-export const baseUrl = 'http://localhost:3082/api';
+export const baseUrl = "http://localhost:3082/api";
 
 export const baseUrlnpc = "http://localhost:3082/api";
 
-export const backendUrl = 'http://localhost:3082';
+export const backendUrl = "http://localhost:3082";
 // export const backendUrl = 'https://gs1ksa.org/api';
-export const gtrackUrl = 'http://gs1ksa.org:7000/api';
+export const gtrackUrl = "http://gs1ksa.org:7000/api";
 // export const gtrackUrl = 'http://localhost:7000/api';
 
 export const vectorEmbeddingUrl = "https://gs1ksa.org:4044/api";
 
-export const npcSubscriberUrl = 'http://localhost:5022/api';
-
+export const npcSubscriberUrl = "http://localhost:5022/api";
