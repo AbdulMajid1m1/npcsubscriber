404013d9f66a8bbef4f7938a2e1588f2d16e41a3
unknown <abdulmajid1m2@gmail.com>
2024-08-23 00:22:31 +0500

    ads

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -2,7 +2,6 @@ export const baseUrl = "http://gs1ksa.org:3082/api";
 
 export const baseUrlnpc = "http://gs1ksa.org:3082/api";
 
-// export const backendUrl = "http://localhost:3082";
 export const backendUrl = 'http://gs1ksa.org:3091';
 
 export const gtrackUrl = "http://gs1ksa.org:7000/api";
