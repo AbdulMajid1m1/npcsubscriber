656ccce7b10ab25d387ca164081cb84eb80baf0e
unknown <abdulmajid1m2@gmail.com>
2024-09-26 17:23:28 +0500

    ad

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,12 +1,14 @@
-export const baseUrl = "http://gs1ksa.org:3082/api";
+export const baseUrl = "http://gs1ksa.org:3083/api";
 
-export const baseUrlnpc = "http://gs1ksa.org:3082/api";
+export const baseUrlnpc = "http://gs1ksa.org:3083/api";
 
-export const backendUrl = 'http://gs1ksa.org:3091';
+export const backendUrl = "http://gs1ksa.org:3091";
 
 export const gtrackUrl = "http://gs1ksa.org:7000/api";
 // export const gtrackUrl = 'http://localhost:7000/api';
 
 export const vectorEmbeddingUrl = "https://gs1ksa.org:4044/api";
 
-export const npcSubscriberUrl = "http://localhost:5022/api";
+// export const npcSubscriberUrl = "http://localhost:5022/api";
+
+export const npcSubscriberUrl = "http://gs1ksa.org:5022/api";
